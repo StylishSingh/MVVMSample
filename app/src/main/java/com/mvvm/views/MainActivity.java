e377ad30db21ce856abbb5db93dc77267e9b7828
Amanpal Singh <amanpal@mobilyte.com>
2017-10-06 17:52:20 +0530

    test files deleted

--- app/src/main/java/com/mvvm/views/MainActivity.java
+++ app/src/main/java/com/mvvm/views/MainActivity.java
@@ -1,7 +1,9 @@
-package com.mvvm;
+package com.mvvm.views;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mvvm.R;
 
 public class MainActivity extends AppCompatActivity {
 
